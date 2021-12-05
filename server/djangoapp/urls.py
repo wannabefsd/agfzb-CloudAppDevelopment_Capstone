2ff74e2733f65fd7a5186bee6bb9e329b28b49d6
wannabefsd <91594754+wannabefsd@users.noreply.github.com>
2021-12-05 12:53:19 +0400

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -10,24 +10,29 @@ urlpatterns = [
     # name the URL
 
     # path for about view
+    path(route='', view=views.get_dealerships, name='index'),
+    #week 1 T4
     path(route='about', view=views.about, name='about'),
-
     # path for contact us view
     path(route='contact', view=views.contact, name='contact'),
 
     # path for registration
-    path('registration/', views.registration_request, name='registration'),
+    #week 2 t7
+    path('registration/', view=views.registration_request, name='registration'),
 
     # path for login
-    path('login/', views.login_request, name='login'),
+    #week 2 t 5
+    path(route='login/', view=views.login_request, name='login'),
 
     # path for logout
-    path('logout/', views.logout_request, name='logout'),
+    #week 2 t 5
+    path(route='logout/', view=views.logout_request, name='logout'),
 
-    path(route='', view=views.get_dealerships, name='index'),
+    
 
     # path for dealer reviews view
-
+    path('dealer/<int:dealer_id>/', view=views.get_dealer_details, name='dealer_details'),
     # path for add a review view
+    path(route='addreview/<int:dealer_id>/', view=views.add_review, name='add_review')
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

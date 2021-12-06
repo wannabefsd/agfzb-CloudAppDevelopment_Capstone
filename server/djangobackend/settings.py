a95528d9bfced4562d65bdcffba021d5d2396c6e
wannabefsd <91594754+wannabefsd@users.noreply.github.com>
2021-12-06 15:00:17 +0400

    Update settings.py

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = 'ao5z(o(z@cvzodm99d32jkxa5e8a1!q_4sqss5-a%n6tg$#h$+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS =  ["127.0.0.1","innovacar.mybluemix.net","localhost"]
+ALLOWED_HOSTS =  ["127.0.0.1","djangoapp-etienneuae.eu-gb.mybluemix.net","localhost"]
 
 
 # Application definition
